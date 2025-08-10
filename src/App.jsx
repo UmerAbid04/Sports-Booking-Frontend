567ede09ad46769a014474567b79591a5495413a
Umer Abid <umerabidisb@gmail.com>
2025-08-10 18:44:42 +0500

    Fix Supabase Google OAuth bridge

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import supabase from './lib/supabaseClient';
 
 
 import HomePage from "./Pages/HomePage";
